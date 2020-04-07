fece6475c704dcad5b8e00e026c641d7c5bf5430
nelson kenzo tamashiro <nelsonkenzotamashiro@gmail.com>
2020-04-07 14:34:55 -0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,108 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import "./styles.css";
+import api from "./services/api";
+
+const projectCard = ({
+    repo,
+    onClickRemove,
+}) => (
+        <div key={repo.id} style={{ width: '300px' }} >
+            <h3>{repo.title}</h3>
+            <p>Link: {repo.url}</p>
+            <p>Techs: {repo.techs ? repo.techs.join(', ') : null}</p>
+            <button onClick={() => onClickRemove(repo.id)}>
+                {'Remover'}
+            </button>
+        </div>
+    )
+
+const parseTechsString = techsString => techsString.replace(/\s/g, '').split(',');
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+    const cleanInputs = () => {
+        titleInput.current.value = '';
+        urlInput.current.value = '';
+        techStringInput.current.value = '';
+    };
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    async function handleAddRepository() {
+        const newProject = {
+            title: titleInput.current.value,
+            url: urlInput.current.value,
+            techs: parseTechsString(techStringInput.current.value),
+        };
+        const { data } = await api.post('repositories', {
+            newProject,
+        });
+        cleanInputs();
+        setProjects([
+            ...projectList,
+            data,
+        ]);
+    }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+    async function handleRemoveRepository(id) {
+        cleanInputs();
+        const { status } = await api.delete(`repositories/${id}`);
+        if (status === 204) {
+            setProjects(projectList.filter(project => project.id !== id));
+        }
+    }
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+    const [projectList, setProjects] = useState([]);
+    const titleInput = useRef();
+    const urlInput = useRef();
+    const techStringInput = useRef();
+
+    return (
+        <div>
+            <h1>{'Lista de Projetos'}</h1>
+            <ul
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                }}
+                data-testid="repository-list"
+            >
+                {projectList.map(
+                    project => projectCard({
+                        repo: project,
+                        onClickRemove: handleRemoveRepository,
+                    })
+                )}
+            </ul>
+
+            <h1>{'Novo Projeto'}</h1>
+            <form>
+                <input
+                    ref={titleInput}
+                    placeholder="Título do projeto" 
+                    type="text"
+                    /> <br />
+                <input 
+                    ref={urlInput}
+                    placeholder="Link" 
+                    type="text"
+                    /> <br />
+                <input 
+                    ref={techStringInput}
+                    placeholder="Techs, separado por vírgula"
+                    type="text"
+                />
+            </form>
+            <button onClick={() => handleAddRepository()}>
+                {'Adicionar'}
+            </button>
+        </div>
+    );
 }
 
 export default App;
